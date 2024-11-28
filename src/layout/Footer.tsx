52f18364322ef4d4cf11d8e2bf8e466d167104fd
Mark <mark@leago.gg>
2024-11-28 00:53:28 -0500

    made the footer look marginally better

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 export const Footer = () => {
   return (
     <div
-      className={`flex justify-around items-center h-[var(--footer-height)] bg-sidebar w-full text-2xl`}
+      className={`flex justify-around items-center h-[var(--footer-height)] bg-sidebar w-full text-2xl mt-6 rounded-lg`}
     >
       <div className='flex flex-1 items-center justify-center'>
         <span>City: Toronto, Ontario</span>
