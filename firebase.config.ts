48ea7419de2a47f0d39c4237d79555a57a0c59cf
Mark <markwong167@gmail.com>
2024-12-02 17:14:00 -0500

    added firebase hosting

---
+++ firebase.config.ts
@@ -0,0 +1,11 @@
+//Not secret info, can be public
+export const firebaseConfig = {
+  apiKey: "AIzaSyCmwhoGXFEoowOGIs4EQ5XMPgAJ95CCZu8",
+  authDomain: "markwongportfolio.firebaseapp.com",
+  databaseURL: "https://markwongportfolio.firebaseio.com",
+  projectId: "markwongportfolio",
+  storageBucket: "markwongportfolio.firebasestorage.app",
+  messagingSenderId: "903455983351",
+  appId: "1:903455983351:web:1375836f18730e8f5df5f2",
+  measurementId: "G-Y4CYN86NSN",
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { initializeApp } from "firebase/app";
+import { firebaseConfig } from "../firebase.config";
+initializeApp(firebaseConfig);
 
 const root = document.getElementById("root");
 if (!root) throw new Error("Root element not found");
