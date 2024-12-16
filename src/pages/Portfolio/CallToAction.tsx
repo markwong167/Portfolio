94905da72819fd60d8f226da545885fb961f9993
Mark <markwong167@gmail.com>
2024-12-15 21:09:08 -0500

    better looking mobile CTA

--- src/pages/Portfolio/CallToAction.tsx
+++ src/pages/Portfolio/CallToAction.tsx
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 import { FaEnvelope, FaLinkedin } from "react-icons/fa";
 import { Card } from "../../components/ui/card";
 export const CallToAction = ({ isMobile }: { isMobile: string }) => {
-  const isMobileOrTablet = ["M", "T"].includes(isMobile);
   let titleTextSize = "text-4xl";
   let bodyTextSize = "text-xl";
   let linkTextSize = "text-xl";
@@ -17,9 +16,17 @@ export const CallToAction = ({ isMobile }: { isMobile: string }) => {
   }
   return (
     <>
-      <Card className='flex flex-col justify-center items-center h-full w-full gap-8 pt-6 pb-12'>
+      <Card
+        className={`flex flex-col justify-center items-center h-full w-full ${
+          isMobile === "M" ? "gap-3 pt-4 pb-6" : "gap-8 pt-6 pb-12"
+        }`}
+      >
         <h2 className={titleTextSize}>Let's work together</h2>
-        <div className='flex flex-row gap-12'>
+        <div
+          className={`flex ${
+            isMobile === "M" ? "flex-col gap-2" : "flex-row gap-12"
+          }`}
+        >
           <Link
             to='https://www.linkedin.com/in/markwong167/'
             target='_blank'
